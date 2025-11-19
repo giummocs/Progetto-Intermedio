0087419f3a9469139e29183a1b8aa39fc0622846
angeloyolo <angelo.you@studenti.unipd.it>
2025-11-19 12:11:00 +0100

    Update InertialDriver.cpp

--- InertialDriver/include/InertialDriver.cpp
+++ InertialDriver/include/InertialDriver.cpp
@@ -6,9 +6,21 @@
 		BUFFER_DIM=0;
 		MyVector v(BUFFER_DIM);
 	}
+	
 	InertialDriver::InertialDriver(int buff_sz)
 	{
 		BUFFER_DIM=buff_sz;
 		MyVector v(BUFFER_DIM);
+	}
+
+	void InertialDriver::push_back(Lettura v[])
+	{
+		v.push;
+		return;
+	}
+	
+	Lettura[] InertialDriver::pop_front()
+	{
+		return v.pop_front();
 	}
 }
