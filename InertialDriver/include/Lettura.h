97b54d845796dac6be69906f8bb1b29ebc8b0100
angeloyolo <angelo.you@studenti.unipd.it>
2025-11-19 10:03:22 +0100

    Add files via upload

---
+++ InertialDriver/include/Lettura.h
@@ -0,0 +1,14 @@
+#ifndef LETTURA_H
+#define LETTURA_H
+
+struct Lettura{
+    double yaw_v;
+    double yaw_a;
+    double pitch_v;
+    double pitch_a;
+    double roll_v;
+    double roll_a;
+};
+
+
+#endif
