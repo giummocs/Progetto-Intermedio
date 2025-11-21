e5a32e0bc967f4a72174f1c4b86792693f8feb32
giummocs <gianmaria.stefani@studenti.unipd.it>
2025-11-21 20:03:39 +0100

    Update InertialDriver.h

--- InertialDriver/include/InertialDriver.h
+++ InertialDriver/include/InertialDriver.h
@@ -20,7 +20,7 @@ class InertialDriver{
 
     //rimozione di una misura
     //void poiche' stampa in output l'array piu' vecchio
-    Lettura* pop_front();
+    const Lettura* pop_front();
 
     //svuota senza restituire nulla il buffer
     void clear_buffer();
@@ -44,6 +44,7 @@ private:
 std::ostream& operator<<(std::ostream& out, const InertialDriver& t);
 
 #endif
+
 
 
 
