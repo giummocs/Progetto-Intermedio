5c1333be2fbd87f5eb2f7cfb0eb4d3888ed993fb
angeloyolo <angelo.you@studenti.unipd.it>
2025-11-21 16:13:50 +0100

    Update Misura.h

--- InertialDriver/include/Misura.h
+++ InertialDriver/include/Misura.h
@@ -5,16 +5,18 @@
 class Misura{
   //definisco una costante pubblica che indica il numero di sensori
   static const int SENSOR= 17;
-  //construttore di default
+  //costruttore di default
   Misura();
+  //costruttore di copia
+  Misura(const Misura& m);
   //
   void replace(Lettura nuova_misura[]);
   //distruttore
   ~Misura();
   
   Lettura& operator[] (int i);
-
-
+  
+  Misura& operator= (const Misura& m);
 
   private:
   Lettura array[SENSOR];
