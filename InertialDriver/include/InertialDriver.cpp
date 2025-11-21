79a0dbe97c98e2837d766307b78863dcdbd5344a
giummocs <gianmaria.stefani@studenti.unipd.it>
2025-11-21 18:44:36 +0100

    Update InertialDriver.cpp

--- InertialDriver/include/InertialDriver.cpp
+++ InertialDriver/include/InertialDriver.cpp
@@ -73,16 +73,7 @@ std::ostream& InertialDriver::print() const{
 	int index_last = (last - 1 + BUFFER_DIM) % BUFFER_DIM;
     
     out << "\nUltima Misura Salvata\n";
-	for(int i = 0; i < 17; i++)
-	{
-	    out << "Sensore " << i + 1 << ":\n";
-		out << "Yaw_v = " << v[index_last][i].yaw_v << "\n";
-		out << "Yaw_a = " << v[index_last][i].yaw_a << "\n";
-	    out << "Pitch_v = " << v[index_last][i].pitch_v << "\n";
-	    out << "Pitch_a = " << v[index_last][i].pitch_a << "\n";
-	    out << "Roll_v = " << v[index_last][i].roll_v << "\n";
-	    out << "Roll_a = " << v[index_last][i].roll_a << "\n\n";
-	}
+	out << v[index_last];
     return out;
 }
 
